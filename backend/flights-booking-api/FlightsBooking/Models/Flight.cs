5775cc3ec4acb0c21572bf2e589dbc9423ce2c7b
Veljic <veljic.ra31.2019@uns.ac.rs>
2023-03-15 09:34:49 +0100

    model created v1

--- backend/flights-booking-api/FlightsBooking/Models/Flight.cs
+++ backend/flights-booking-api/FlightsBooking/Models/Flight.cs
@@ -1,29 +1,77 @@
+using FlightsBooking.Support.ErrorHandler.Model;
 using System.ComponentModel.DataAnnotations;
 
 namespace FlightsBooking.Models
 { 
     public class Flight
     {
-        [Required]
+        [Key]
         public Guid FlightId { get; set; }
-        [Required]
-        public FlightTickets FlightTickets { get; set; }
 
         [Required]
         public Departure Departure { get; set; }
         [Required]
         public Arrival Arrival { get; set; }
+        [Required]
+        public int TotalTickets { get; set; }
+        [Required]
+        public double TicketPrice { get; set; }
+        [Required]
+        public List<SoldTicket> SoldTickets { get; set; }
 
         [Required]
-        public bool Passed { get; set; }
+        public bool IsDeleted { get; set; }
 
-        public Flight(Guid flightId, FlightTickets flightTickets, Departure departure, Arrival arrival, bool passed)
+        public Flight(Departure departure, Arrival arrival, int totalTickets, double ticketPrice, List<SoldTicket> soldTickets)
         {
-            FlightId = flightId;
-            FlightTickets = flightTickets;
+            ValidateTicketPrice(ticketPrice);
+            ValidateTotalTicketsCount(totalTickets);
+            CheckIfDepartureIsBeforeArrival(departure, arrival);
+            FlightId = Guid.NewGuid();
             Departure = departure;
             Arrival = arrival;
-            Passed = passed;
+            TotalTickets = totalTickets;
+            TicketPrice = ticketPrice;
+            SoldTickets = soldTickets;
+            IsDeleted = false;
+        }
+
+        private void CheckIfDepartureIsBeforeArrival(Departure departure, Arrival arrival)
+        {
+            if(departure.Time > arrival.Time)
+            {
+                throw new DepartureIsAfterArrivalException();
+            }
+        }
+
+        private void ValidateTotalTicketsCount(int totalTickets)
+        {
+            if (totalTickets < 0)
+                throw new InvalidTotalTicketsCountException();
+        }
+
+        private void ValidateTicketPrice(double ticketPrice)
+        {
+            if (ticketPrice < 0)
+                throw new InvalidPriceException();
+        }
+
+        public bool IsFlightPassed()
+        {
+            return this.Departure.Time < DateTime.UtcNow;
+        }
+        public double CalculateTotalPriceForFlight()
+        {
+            double totalPrice = 0;
+            foreach (var soldTicket in this.SoldTickets)
+            {
+                totalPrice += soldTicket.Price;
+            }
+            return totalPrice;
+        }
+        public int CalculateNumberOfAvailableTicketForTheFlight()
+        {
+            return this.TotalTickets - this.SoldTickets.Count();
         }
     }
 }
