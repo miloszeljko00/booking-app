d750761634efad4a27d3913644f7d084caf04cd1
Nikola Holjevac <holjevac.ra11.2019@uns.ac.rs>
2023-05-14 01:08:10 +0200

    Backend for guest notifications
    Review and management of notification settings, communication between microservices using gRPC, sending mails for request approval

--- backend/Accomodation/Application/Accommodation/Commands/ManageReservationRequestCommandHandler.cs
+++ backend/Accomodation/Application/Accommodation/Commands/ManageReservationRequestCommandHandler.cs
@@ -1,9 +1,11 @@
-﻿using Accomodation.Domain.Primitives.Enums;
+﻿using AccommodationApplication.Accommodation.Support.Grpc.Protos;
+using Accomodation.Domain.Primitives.Enums;
 using AccomodationApplication.Abstractions.Messaging;
 using AccomodationDomain.Entities;
 using AccomodationDomain.Interfaces;
 using AccomodationDomain.Primitives.Enums;
 using AccomodationDomain.ValueObjects;
+using Grpc.Core;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +17,11 @@ namespace AccomodationApplication.Accommodation.Commands
     public sealed class ManageReservationRequestCommandHandler : ICommandHandler<ManageReservationRequestCommand, AccomodationDomain.Entities.Accommodation>
     {
         private readonly IAccommodationRepository _repository;
+
+        private Channel channel;
+
+        private GuestNotificationGrpcService.GuestNotificationGrpcServiceClient client;
+
         public AccommodationBuilder AccommodationBuilder { get; set; } = new AccommodationBuilder();
         public ManageReservationRequestCommandHandler(IAccommodationRepository repository)
         {
@@ -26,7 +33,7 @@ namespace AccomodationApplication.Accommodation.Commands
             return _repository;
         }
 
-        public Task<AccomodationDomain.Entities.Accommodation> Handle(ManageReservationRequestCommand request, CancellationToken cancellationToken)
+        public async Task<AccomodationDomain.Entities.Accommodation> Handle(ManageReservationRequestCommand request, CancellationToken cancellationToken)
         {
             var acc = _repository.GetAsync(request.reservationRequestManagementDTO.AccommodationId);
             AccomodationDomain.Entities.Accommodation accommodation = acc.Result;
@@ -45,11 +52,12 @@ namespace AccomodationApplication.Accommodation.Commands
                     break;
                 }
             }
-
+            string operation;
             if (req is not null)
             {
                 if (request.reservationRequestManagementDTO.Operation.Equals("ACCEPT"))
                 {
+                    operation = "PRIHVACEN";
                     List<ReservationRequest> reqs = accommodation.GetReservationRequestsOverlappingDateRange(req.ReservationDate);
                     foreach (ReservationRequest reservationRequest in reqs)
                     {
@@ -61,6 +69,7 @@ namespace AccomodationApplication.Accommodation.Commands
                 }
                 else if (request.reservationRequestManagementDTO.Operation.Equals("REJECT"))
                 {
+                    operation = "ODBIJEN";
                     accommodation.CreateReservationRequest(req.GuestEmail.EmailAddress, req.ReservationDate.Start, req.ReservationDate.End, req.GuestNumber, ReservationRequestStatus.REJECTED);
                 }
                 else
@@ -71,8 +80,13 @@ namespace AccomodationApplication.Accommodation.Commands
             else
                 throw new Exception("Reservation request does not exist");
 
+            channel = new Channel("127.0.0.1:8787", ChannelCredentials.Insecure);
+            client = new GuestNotificationGrpcService.GuestNotificationGrpcServiceClient(channel);
+            MessageResponseProto response = await client.communicateAsync(new MessageProto() { Email = req.GuestEmail.EmailAddress, Accommodation = accommodation.Name, StartDate = req.ReservationDate.Start.ToString("dd.MM.yyy."), EndDate = req.ReservationDate.End.ToString("dd.MM.yyy."), Operation = operation });
+            Console.WriteLine(response);
+
             _repository.UpdateAsync(accommodation.Id, accommodation);
-            return Task.FromResult(accommodation);
+            return accommodation;
         }
     }
 }
---
+++ backend/Accomodation/Notification.Application/Notification/Support/Email/IEmailService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notification.Application.Notification.Support.Email
+{
+    public interface IEmailService
+    {
+        void SendGuestNotification(string email, string operation, string accommodation, string startDate, string endDate);
+    }
+}
