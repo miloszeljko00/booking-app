2961aff159ff0e36c87180e346098e68ef7c2213
Nikola Holjevac <holjevac.ra11.2019@uns.ac.rs>
2023-05-16 11:52:31 +0200

    Settings for permanent toastr notification (needed for highlighted host)

--- accommodation-frontend/src/app/pages/home/home.page.ts
+++ accommodation-frontend/src/app/pages/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/core/keycloak/auth.service';
+import { User } from 'src/app/core/keycloak/model/user';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.page.scss']
 })
 export class HomePage {
+  user!:User | null;
 
+  constructor(private toastr : ToastrService, private authService: AuthService) {
+  }
+  ngOnInit() {
+    this.user = this.authService.getUser()
+    if(this.user?.roles.includes('host')){
+      this.toastr.clear()
+      this.toastr.info('DODELJEN VAM JE STATUS ISTAKNUTOG HOSTA!', '', {"timeOut":0, "extendedTimeOut":0, tapToDismiss:false, "positionClass":'toast-bottom-left'})
+    }
+  }
 }
