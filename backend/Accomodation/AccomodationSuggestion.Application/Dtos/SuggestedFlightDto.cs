b78c2962ac9fc9f2c8a4ddaa167b1b248e21abfd
Veljic <veljic.ra31.2019@uns.ac.rs>
2023-06-11 18:53:41 +0200

    wip 1

---
+++ backend/Accomodation/AccomodationSuggestion.Application/Dtos/SuggestedFlightDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccomodationSuggestion.Application.Dtos
+{
+    public class SuggestedFlightDto
+    {
+        public DateTime DepartureDate { get; set; }
+        public DateTime ArrivalDate { get; set; }
+        public double TicketPrice { get; set; }
+    }
+}
---
+++ backend/Accomodation/AccomodationSuggestion.Application/Suggestion/Queries/GetAllSuggestedFlightsQuery.cs
@@ -0,0 +1,16 @@
+﻿using AccomodationApplication.Abstractions.Messaging;
+using AccomodationSuggestion.Application.Dtos;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccomodationSuggestion.Application.Suggestion.Queries
+{
+    public sealed record GetAllSuggestedFlightsQuery(GetAllSugestedFlightsDTO queryDto) : IQuery<GetAllSuggestedFlightsResponse>
+    {
+
+    }
+}
